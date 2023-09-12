48e182349d95c27f7c09ab09ca649ee1e66cdc90
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2023-09-12 06:28:09 +0200

    Update bf.c

--- bf.c
+++ bf.c
@@ -1,1 +1,1 @@
-s[99],*r=s,*d,c;main(a,b){char*v=1[d=b];for(;c=*v++%93;for(b=c&2,b=c%7?a&&(c&17?c&1?(*r+=b-1):(r+=b-1):syscall(4-!b,b,r,1),0):v)b&&c|a**r;v=d)main(!c,&a);d=v;}
+s[99],*r=s,*d,c;main(a,b){char*v=1[d=b];for(;c=*v++%93;for(b=c&2,b=M(r,v))b&&c|a**r;v=d)main(!c,&a);d=v;}
